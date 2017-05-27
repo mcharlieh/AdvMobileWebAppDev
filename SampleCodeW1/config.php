5b4acd81915e36a44a987719bda1310970e689e5
Megan Haley <megan.haley@smail.rasmussen.edu>
2017-05-27 14:08:02 -0500

    Week2 Upload
    Test change to upload initial files

---
+++ SampleCodeW1/config.php
@@ -0,0 +1,7 @@
+<?php
+// This is an example of config.php
+$dbhost = 'us-cdbr-azure-central-a.cloudapp.net';
+$dbuser = 'b897fbb66208c1';
+$dbpass = '66f65acb';
+$dbname = 'acsm_281b165bef9e739';
+?>
